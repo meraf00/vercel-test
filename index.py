a55bf466528d3e14abdeff3ce6815af08b969ae1
meraf00 <meraf000@gmail.com>
2022-10-01 10:06:10 +0300

    Removed Flask Run

--- index.py
+++ index.py
@@ -12,7 +12,3 @@ app.config["SECRET_KEY"] = os.getenv("SECRET")
 @app.route("/")
 def home():
     return render_template("home.html", env=os.getenv("ENV"), prettyprint=prettyprint)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
